e6cf475fe06713babb2fed095af23d879ebb72b2
jin sun kyoung <jinsk9268@gmail.com>
2020-10-13 01:53:53 +0900

    Add 커스텀 Hooks 추가 전

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './practice-1/App';
-import App from './practice-2/App';
+// import App from './practice-2/App';
+import App from './practice-3/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/practice-3/CreateUser.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const CreateUser = ({ username, email, onChange, onCreate }) => {
+  return (
+    <div>
+      <input
+        placeholder='계정명'
+        name='username'
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        placeholder='이메일'
+        name='email'
+        onChange={onChange}
+        value={email}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
+};
+
+export default React.memo(CreateUser);
+
+// React.memo
+// 컴포넌트 리렌더링 방지를 위해 사용
+// 컴포넌트의 props가 바뀌지 않았다면, 리렌더링을 방지하여
+// 컴포넌트의 리렌더링 성능 최적화를 해줄수 있다
+// 이 함수를 사용하면 컴포넌트에서 리렌더링이 필요한 상황에서만 리렌더링 하도록 설정할 수 있다
+// export default React.memo(CreateUser); 이 형식으로 감싸 주면 된다
